387833c94c463646a2b7c8f523cd44a76170a4a5
AryanBarhanpure <aryanbarhanpure9999@gmail.com>
2025-04-18 10:53:39 +0530

    Add files via upload

---
+++ E01.java
@@ -0,0 +1,12 @@
+public class E01 {
+    public static void main(String[] args) {
+        try{
+            int a = 5,b=0,c;
+            c = a/b;
+            System.out.println("The Answer of Sol : ");
+        }
+        catch (Exception e){
+            System.out.println("The Exception is "+e);
+        }
+    }
+}
